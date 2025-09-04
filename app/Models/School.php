8539450dd1f4b81f0456c1298a633ab40205744a
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-09-04 12:16:09 +0700

    fillable deleted_at on school

--- app/Models/School.php
+++ app/Models/School.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class School extends Model
 {
-    protected $fillable = ["name"];
+    protected $fillable = ["name", "deleted_at"];
 
     public $timestamps = false;
 }
