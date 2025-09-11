8491742c3ca379182e38f982487a3b0d28616eaf
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-09-11 09:49:44 +0700

    qadd leader and other teams

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -6,22 +6,27 @@ class TeamController extends Controller
 {
     public function index()
     {
+        $leader = [
+            'name' => "Galih Purnamasari, S.Gz, M.Si.",
+            'image' => "https://kesehatan.polije.ac.id/img/foto/44%20Galih.JPG",
+        ];
+
         $teams = [
             [
-                'name' => "Dahlia Indah Amareta, S.KM, M.Gizi",
+                'name' => "Dahlia Indah Amareta, S.KM., M.Kes./Gizi Masyarakat",
                 'image' => "https://kesehatan.polije.ac.id/img/foto/2%20Dahlia%20Indah%20Amareta.jpg",
             ],
             [
-                'name' => "Galih Purnamasari, S.Gz, M.Si",
-                'image' => "https://kesehatan.polije.ac.id/img/foto/44%20Galih.JPG",
+                'name' => "Alinea Dwi Elisanti, S.KM., M.Kes./Kesehatan Masyarakat",
+                'image' => "https://kesehatan.polije.ac.id/img/foto/38%20Alinea.JPG",
             ],
             [
-                'name' => "Efri Tri Ardianto, S.KM, M.Kes",
+                'name' => "Efri Tri Ardianto, S.KM., M.Kes./Statistik Kesehatan",
                 'image' => "https://kesehatan.polije.ac.id/img/foto/19%20Efri%20Tri.jpg",
             ],
             [
-                'name' => "Alinea Dwi Elisanti, S.KM, M.Kes",
-                'image' => "https://kesehatan.polije.ac.id/img/foto/38%20Alinea.JPG",
+                'name' => "Agustina Endah Werdiharini, S.Sos., M.Kes./Kesehatan Masyarakat",
+                'image' => "https://kesehatan.polije.ac.id/img/foto/12%20Endah.jpg",
             ]
         ];
 
@@ -29,6 +34,7 @@ class TeamController extends Controller
             'success' => true,
             'message' => "Data tim berhasil diambil",
             'data' => [
+                'leader' => $leader,
                 'teams' => $teams,
             ]
         ];
